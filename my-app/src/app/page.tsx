a64c17626584a485f7e94b5e3dd4cf5e4d8cf2db
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-11-11 15:58:19 +0530

    added basic folder structure

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+import './page.css'
+
+export default function Home() {
+  return (
+    <div className="HomeComponent">
+      <div className="HomeComponent-in">
+        <h1>Smart Village Revolution</h1>
+      </div>
+    </div>
+  );
+}

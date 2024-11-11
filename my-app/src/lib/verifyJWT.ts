6166f1ba87430b087e740502e151957eacf9e48f
rekha <rekhans321@gmail.com>
2024-11-11 17:03:35 +0530

    authentication added

---
+++ my-app/src/lib/verifyJWT.ts
@@ -0,0 +1,31 @@
+import { cookies } from 'next/headers'
+import { jwtVerify } from "jose";
+
+export const verifyJWT = async () => {
+    try {
+        const cookieStore = cookies()
+        const JWT = cookieStore.get('jwt')?.value;
+
+        if (!JWT) {
+            return { valid: false, reason: 'JWT missing' };
+        }
+
+        const { payload } = await jwtVerify(
+            JWT,
+            new TextEncoder().encode(process.env.REFRESH_TOKEN_SECRET),
+            {
+                algorithms: ['HS256'],
+            }
+        );
+
+        console.log('payload', payload)
+
+        if (payload) {            
+            return { valid: true, payload: payload };
+        } else {
+            return { valid: false, reason: 'Invalid JWT' };
+        }
+    } catch (error) {
+        return { valid: false, reason: 'Verification error' };
+    }
+}

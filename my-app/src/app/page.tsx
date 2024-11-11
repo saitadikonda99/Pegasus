a351e60eb651f6b54105d67b28f631e3d1ddbe38
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-11-11 16:54:30 +0530

    swtiched from next 15 to next 14

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,10 @@
 
-
-import './page.css'
-
 export default function Home() {
   return (
-    <div className="HomeComponent">
-      <div className="HomeComponent-in">
-        <h1>Smart Village Revolution</h1>
+      <div className="HomeComponent">
+        <div className="HomeComponent-in">
+          <h1>Smart Village Revolution</h1>
+        </div>
       </div>
-    </div>
   );
 }
